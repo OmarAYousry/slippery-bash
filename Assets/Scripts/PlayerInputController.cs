d2e0f36e2591602e94279362926f5a3974eceb39
Omar Ahmed <omarahm3d19@gmail.com>
2020-12-13 10:24:06 +0200

    [feature] Add PlayerBehaviour and PlayerInputController Scripts

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : MonoBehaviour
+{
+    public void OnNavigate()
+    {
+        Debug.Log("Navigating...");
+    }
+
+    public void OnSubmit()
+    {
+        Debug.Log("Submitting...");
+        GetComponent<PlayerInput>().SwitchCurrentActionMap("Player");
+    }
+
+    public void OnPunch()
+    {
+        GetComponent<PlayerInput>().SwitchCurrentActionMap("UI");
+        Debug.Log("Shots fired!!!");
+    }
+
+    public void OnPause()
+    {
+        Debug.Log("Pausing...");
+    }
+
+    public void OnResume()
+    {
+        Debug.Log("Resuming...");
+    }
+
+    public void OnJoin()
+    {
+        Debug.Log("Joining...");
+    }
+
+    public void OnLeave()
+    {
+        Debug.Log("Leaving...");
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Debug.Log("Moving..." + value.Get<Vector2>());
+    }
+}
