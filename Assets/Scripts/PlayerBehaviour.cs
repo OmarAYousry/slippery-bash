f25bd69d1cdb9bdd82e0cdf94a4b6012219546f2
Omar Ahmed <omarahm3d19@gmail.com>
2020-12-15 17:12:53 +0200

    Add new testing assets and improve existing prefabs

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -2,5 +2,13 @@
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    [SerializeField]
+    private PlayerInputController playerInputController = null;
+    [SerializeField]
+    private Rigidbody playerRigidbody = null;
 
+    public void MovePlayer(Vector2 moveDirection)
+    {
+
+    }
 }
