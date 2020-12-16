4df3cc20ebde2beb9ab64f7e4dd64a29d0b513a2
Tarek ElSherif <tarek.elsherif9@gmail.com>
2020-12-16 18:14:52 +0100

    Added initial assets for Buoyancy and Destruction testing

---
+++ Assets/Scripts/ClickTester.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ClickTester : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                TileController tile = hit.collider.gameObject.GetComponent<TileController>();
+                if (tile != null)
+                {
+                    tile.DestroyMesh();
+                }
+            }
+        }
+    }
+}
