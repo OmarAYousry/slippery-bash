f75773c333d8a8cb284b1ca4a6d3dd4bce06c9ee
Omar Ahmed <omarahm3d19@gmail.com>
2020-12-18 18:49:42 +0200

    [feature] Add GameController and TitleBehaviour classes to better control game flow

---
+++ Assets/Scripts/TitleBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TitleBehaviour : MonoBehaviour
+{
+    private static TitleBehaviour instance = null;
+
+    private void Awake()
+    {
+        gameObject.SetActive(true);
+        instance = this;
+    }
+
+
+    public static void PrepareGameLobby()
+    {
+        
+    }
+
+    public static void DisableTitleScreen()
+    {
+        instance.gameObject.SetActive(false);
+    }
+}
