2306045c3f1cc33fc0f6f944572c9e6f0187abe4
ga45pum <moritz.naser@tum.de>
2021-01-17 21:07:05 +0100

    tiles + randomness

--- Assets/Art/Tiles/ChooseRandomMaterial.cs
+++ Assets/Art/Tiles/ChooseRandomMaterial.cs
@@ -2,21 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ApplyMaterial : MonoBehaviour
+public class ChooseRandomMaterial : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Material newMaterial;
-
+    public List<Material> materials = new List<Material>();
     void Awake()
     {
-        foreach (Transform child in transform)
-        {
-            child.GetComponent<Renderer>().material = newMaterial;
-        }
+        GetComponent<Renderer>().material = materials[(int)Random.Range(0f, materials.Count)];
     }
+
     void Start()
-    {
-        
+    { 
+
     }
 
     // Update is called once per frame
