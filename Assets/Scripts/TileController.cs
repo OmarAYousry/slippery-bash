4b84e8ab6a413078b481e0e8efed1dda3b3c9d87
Tarek ElSherif <tarek.elsherif9@gmail.com>
2021-01-25 00:00:40 +0100

    [feature] added dynamic buoyancy for separated platforms

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -11,17 +11,34 @@ public class TileController : MonoBehaviour
     [SerializeField]
     ParticleSystem destroyEffect;
     [SerializeField]
-    float explosionForce = 100f;
+    float tilePieceMass = 0.01f;
+    [SerializeField]
+    float explosionForce = 10f;
     [SerializeField]
     int hitsTillDestroy = 3;
 
+    [Header("Neighbor Tiles")]
+    public TileController upTile;
+    public TileController downTile;
+    public TileController rightTile;
+    public TileController leftTile;
+
     int hitsTaken = 0;
     bool destroyed = false;
+    System.Action onDestroy = null;
+
+    const float neighborRayDistance = 3f;
+
+    public bool isDestroyed
+    {
+        get { return destroyed; }
+    }
 
     void Reset()
     {
         tileMesh = transform.GetChild(0).gameObject;
         brokenMeshParent = transform.GetChild(1).gameObject;
+        LocateNeighboringTiles();
     }
 
     public IEnumerator DestroyMesh(float secondsToWait, bool scatterPieces = false)
@@ -43,13 +60,81 @@ public class TileController : MonoBehaviour
             {
                 brokenPiece.gameObject.AddComponent<TimedDestroyer>();
                 Rigidbody rb = brokenPiece.gameObject.AddComponent<Rigidbody>();
-                rb.mass = 0.1f;
+                rb.mass = 0.001f;
                 float randomFloat = Random.Range(1f, 2f);
                 rb.AddForce(Vector3.up * explosionForce);
+            }
+
+            PlatformController.instance.SplitTileGroups();
+
+            if (onDestroy != null)
+            {
+                onDestroy();
+                onDestroy = null;
             }
         }
 
         yield return null;
+    }
+
+    void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+
+    public List<TileController> GetNeightboringTiles()
+    {
+        List<TileController> neightbors = new List<TileController>();
+        if (upTile != null)
+            if (!upTile.destroyed)
+                neightbors.Add(upTile);
+        if (downTile != null)
+            if (!downTile.destroyed)
+                neightbors.Add(downTile);
+        if (rightTile != null)
+            if (!rightTile.destroyed)
+                neightbors.Add(rightTile);
+        if (leftTile != null)
+            if (!leftTile.destroyed)
+                neightbors.Add(leftTile);
+        return neightbors;
+    }
+
+    public void SeperateTileFromParent(Transform newParent = null)
+    {
+        if (transform.parent == null)
+            return;
+
+        transform.parent = newParent;
+
+        if (newParent == null)
+            gameObject.AddComponent<Rigidbody>().mass = 5f;
+
+        OceanSimpleBuoyancy buoyancy = gameObject.GetComponent<OceanSimpleBuoyancy>();
+        if (buoyancy == null)
+            buoyancy = gameObject.AddComponent<OceanSimpleBuoyancy>();
+
+        buoyancy.buoyancyForce = 50f;
+        buoyancy.depthFactor = 1;
+        buoyancy.surfaceOffset = 1f;
+    }
+
+    public void LocateNeighboringTiles()
+    {
+        Quaternion oldRot = transform.rotation;
+        transform.rotation = Quaternion.identity;
+
+        RaycastHit hitUp, hitDown, hitRight, hitLeft;
+        if (Physics.Raycast(transform.position, transform.forward, out hitUp, neighborRayDistance))
+            upTile = hitUp.collider.gameObject.GetComponent<TileController>();
+        if (Physics.Raycast(transform.position, -transform.forward, out hitDown, neighborRayDistance))
+            downTile = hitDown.collider.gameObject.GetComponent<TileController>();
+        if (Physics.Raycast(transform.position, transform.right, out hitRight, neighborRayDistance))
+            rightTile = hitRight.collider.gameObject.GetComponent<TileController>();
+        if (Physics.Raycast(transform.position, -transform.right, out hitLeft, neighborRayDistance))
+            leftTile = hitLeft.collider.gameObject.GetComponent<TileController>();
+
+        transform.rotation = oldRot;
     }
 
     public void DamageTile(float waitTime)
@@ -99,7 +184,7 @@ public class TileController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        //Debug.LogError(collision.gameObject);
+
     }
 
     private void OnTriggerExit(Collider other)
